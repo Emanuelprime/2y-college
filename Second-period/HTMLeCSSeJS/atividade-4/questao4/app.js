096fbad81e176c4678e2c1dc50bb649939b8ca42
Emanuelprime <emanuelll3000@gmail.com>
2024-04-26 11:27:43 -0300

    att

--- Second-period/HTMLeCSSeJS/atividade-4/questao4/app.js
+++ Second-period/HTMLeCSSeJS/atividade-4/questao4/app.js
@@ -1,12 +1,20 @@
-document.getElementById("vetor")
-formulario.addEventListener('submit', function(evento){evento.preventDefault();
-
-    let entrada = document.getElementById('numeros').value;
-    let numeros = entrada.split(',').map(Number);
-    let soma = numeros.reduce((total, numero) => total + numero, 0);
-    let media = soma/numeros;
+document.getElementById("vetor").addEventListener('submit', function(evento){evento.preventDefault();
+const numerosString = document.getElementById("numeros").value;
+const numerosArray = numerosString.split(",").map(num => Number(num));
+const media = calcularMedia(numerosArray);
+    
 
     alert("A média dos numeros é: " + media);
 
 
     });
+
+function calcularMedia(numeros){
+
+    if(numeros.length === 0){
+        return 0;
+    }
+
+    const soma = numeros.reduce((acumulador, numero) => acumulador + numero, 0);
+    return soma / numeros.length;
+}
---
+++ Second-period/HTMLeCSSeJS/atividade-4/questao5/app.js
@@ -0,0 +1,10 @@
+document.getElementById("formulario").addEventListener('submit', function(evento){evento.preventDefault();
+    let letraEscrita = document.getElementById("letra").value;
+
+    if (numero ===''){
+        alert('Digite um numero');
+        return false;
+    }
+
+
+});
