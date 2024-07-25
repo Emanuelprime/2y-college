c4c39bfc794ce65fedc03b9419b612c4647e4450
Emanuelprime <emanuelll3000@gmail.com>
2024-07-25 20:26:19 -0300

    att

--- Second-period/bim1/musica/music/src/components/Artista.js
+++ Second-period/bim1/musica/music/src/components/Artista.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import Image from './images/bbnos'
 function Artista(){
-    return <h2> Página do Artista</h2>;
+    return(
+    <div>
+        <h2> Página do Artista</h2>
+        <h1>BBno$</h1>
+        <img src={bbnos}/>
+    </div>
+        
+    );
 }
 
 export default  Artista;
