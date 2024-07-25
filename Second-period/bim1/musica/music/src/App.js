2ce5116728fdc090bbe083a277d4c00f9288bb42
Emanuelprime <emanuelll3000@gmail.com>
2024-07-25 19:58:05 -0300

    att

--- Second-period/bim1/musica/music/src/App.js
+++ Second-period/bim1/musica/music/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './components/Home';
+import Artista from './components/Artista';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/artista">Artista</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/artista">
+            <Artista />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
