63f7cc633177a710b9db2e754f3dc7bc26e941a0
Emanuelprime <emanuelll3000@gmail.com>
2024-08-05 21:53:14 -0300

    att

--- Second-period/bim1/aula02/react-nutri/src/App.js
+++ Second-period/bim1/aula02/react-nutri/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect , useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+function App(){
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(()=>{
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+    fetch (url) //envia requisição por url
+    .then((r)=> r.json())//recebe a resposta em json
+    .then((json) => setNutri(json))//salva na const nutri pelo metodo SetNutri
+  }, []);
+
+  return(
+    <div>
+      <header>
+        <strong>React Nutri</strong>
       </header>
+
+      {nutri.map((item)=>{
+        return(
+          <article key={item.id}>
+            <strong>{item.titulo}</strong><br/>
+            <img src={item.capa} alt={item.titulo}/><br/>
+            <p>{item.subtitulo}</p><br/>
+            <button>Acessar</button>
+          </article>
+        );
+      })}
     </div>
   );
 }
