734b24ed70fcaedb27a62caa708210a1f024d4fa
Emanuelprime <emanuelll3000@gmail.com>
2024-08-07 14:18:32 -0300

    att

--- Second-period/bim1/aula02/react-nutri/src/App.js
+++ Second-period/bim1/aula02/react-nutri/src/App.js
@@ -1,30 +1,29 @@
-import React, { useEffect , useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App(){
+function App() {
   const [nutri, setNutri] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
 
-    fetch (url) //envia requisição por url
-    .then((r)=> r.json())//recebe a resposta em json
-    .then((json) => setNutri(json))//salva na const nutri pelo metodo SetNutri
+    fetch(url)
+      .then((r) => r.json())
+      .then((json) => setNutri(json));
   }, []);
 
-  return(
-    <div
-    >
+  return (
+    <div>
       <header>
-        <strong>React Nutri</strong>
+        React Nutri
       </header>
 
-      {nutri.map((item)=>{
-        return(
-          <article key={item.id}>
-            <strong>{item.titulo}</strong><br/>
-            <img src={item.capa} alt={item.titulo}/><br/>
-            <p>{item.subtitulo}</p><br/>
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className={`article article-${item.id}`}>
+            <strong>{item.titulo}</strong><br />
+            <img src={item.capa} alt={item.titulo} /><br />
+            <p>{item.subtitulo}</p><br />
             <button>Acessar</button>
           </article>
         );
