b7e65dd455737412a82fe54711346bf268c2e651
Emanuelprime <emanuelll3000@gmail.com>
2024-08-07 21:51:38 -0300

    att

--- Second-period/bim1/Aula03/atividade/src/App.js
+++ Second-period/bim1/Aula03/atividade/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 function App() {
-  const generateQyote = () => {
+  const generateQuote = () => {
     fetch ('https://api.kanye.rest/')
     .then( res => res.json())
     .then(data =>dailyQuote(data))
@@ -27,3 +27,5 @@ function App() {
   );
 
 }
+
+export default App;
