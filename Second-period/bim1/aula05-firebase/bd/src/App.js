44e377f8775c78aa7590a1e19dde8f877eb87321
Emanuelprime <emanuelll3000@gmail.com>
2024-08-28 22:09:14 -0300

    att

--- Second-period/bim1/aula05-firebase/bd/src/App.js
+++ Second-period/bim1/aula05-firebase/bd/src/App.js
@@ -28,6 +28,11 @@ onAuthStateChanged
 
   const[email, setEmail] = useState('');
   const[senha, setSenha] = useState('');
+
+  const[usuario, setUsuario] = useState(false);
+  const[detalhesUsuario, setDetalhesUsuario] = useState({});
+  
+
   const[post, setPost] = useState([]);
 
   useEffect(()=>{
@@ -49,6 +54,71 @@ onAuthStateChanged
     }
     carregarPost();
   }, [])
+
+  useEffect(() =>{
+    async function verificarLogin(){
+      
+      onAuthStateChanged(auth, (user) => {
+        if(user){
+          //tem usuario logado
+          setUsuario(true);
+          setDetalhesUsuario({
+            uid: user.uid,
+            email: user.email
+          })
+        } else{
+          //não possui usuario logado
+          setUsuario(false);
+          setDetalhesUsuario({});
+        }
+      })
+
+    }
+    verificarLogin();
+  },[])
+
+  async function novoUsuario(){
+   await createUserWithEmailAndPassword(auth, email, senha)
+   .then(() =>{
+    alert("Usuario cadastrado com sucesso!")
+    setEmail("");
+    setSenha("");
+   }).catch((error)=>{
+    if(error.code === 'auth/weak-password'){
+      alert("Senha muito fraca!")
+    }else if(error.code === 'auth/email-already-in-use'){
+      alert("Email já utilizado!")
+    }
+   })
+  }
+
+  async function logarUsuario(){
+    await signInWithEmailAndPassword(auth,email,senha)
+    .then((value) =>{
+      alert("Usuario logado com sucesso!")
+
+      setUsuario(true);
+      setDetalhesUsuario({
+        uid: value.user.uid,
+        email: value.user.email
+      });
+
+      setEmail("");
+      setSenha("");
+    }).catch((error)=>{
+      if(error.code === 'auth/invalid-email'){
+        alert("Email incorreto!")
+      }else if(error.code === 'auth/invalid-password'){
+        alert("Senha incorreta!")
+      }
+     })
+  }
+
+  async function fazerLogout(){
+    await signOut(auth)
+    setUsuario(false)
+    setDetalhesUsuario({})
+  }
 
   //C - create
   async function adicionarPosts(){   //.catch pega os erros, .then executa quando(apos) o addDoc
@@ -117,6 +187,42 @@ onAuthStateChanged
   return(
     <div>
       <h1>ReactJS + Firebase</h1>
+
+      {usuario && (
+        <div>
+          <strong>Seja bem-vindo(a)</strong>
+          <br/>
+          <span>ID: {detalhesUsuario.uid}</span>
+          <br/>
+          <span>Email: {detalhesUsuario.email}</span>
+          <br/>
+          <button onClick={fazerLogout}>Sair</button>
+        </div>
+      )}
+
+      {!usuario &&(
+        <div>
+          <h2>Usuários</h2>
+
+          <label>Email:</label>
+          <input type="email"
+          placeholder="Digite seu email"
+          value={email} onChange={ (e) => setEmail(e.target.value)}/>
+
+          <label>Senha:</label>
+          <input type="password"
+          placeholder="Digite uma senha"
+          value={senha} onChange={ (e) => setSenha(e.target.value)}/>
+
+          <button onClick={novoUsuario}>Cadastrar</button>
+          <button onClick={logarUsuario}>Login</button>
+          </div>
+      )}
+
+      
+
+      <hr></hr>
+
 
       <h2>POSTS</h2>
       <label>ID do Post</label>
